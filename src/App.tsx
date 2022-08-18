35a8025f2ebe01f996b0f6ad2076cdfc095d75ba
hanzii <pwe1ewq@gmail.com>
2022-08-19 02:42:58 +0900

    gestures2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 //framer motion 설치
 // npm i framer-motion
@@ -19,9 +20,20 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 30px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
 `;
 
 // Variants은 컴포넌트가 가질 수 있는 미리 정의된 시각적 state
@@ -31,22 +43,13 @@ const Box = styled(motion.div)`
 // initial 초기값 에니메이션의 초기 스타일
 // animate 최종 스타일
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 // 자식요소의 딜레이까지 조절할 수 있음
 // delayChildren :  딜레이 시간(초) 후에 하위 애니메이션이 시작
 // staggerChildren : 하위 컴포넌트의 애니메이션에 지속 시간(초)만큼 시차를 둘 수 있다
 // 예를 들어, staggerChildren이 0.01이면 첫 번째 자식은 0초, 두 번째 자식은 0.01초, 세 번째 자식은 0.02초 지연되는 식
 const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
+  hover: { rotateZ: 90 },
+  click: { borderRadius: "100px" },
 };
 
 //x,y는 motion에서만 사용 되고 좌표를 나타냄
@@ -59,19 +62,31 @@ const boxVariants = {
 // whileTap 컴포넌트를 누르고 있는 동안 애니메이션할 속성 또는 변형 레이블
 // drag 끌기를 활성화
 // whileDrag 드래그 제스처가 인식되는 동안 애니메이션할 속성 또는 변형 레이블
+
+//drag = x ,y 하면 x 축으로만 움직이고 y축으로만 움직임
+
+//dragConstraints
+// 허용된 드래그 가능 영역에 제약 조건을 적용
+
+// ref 이용
+// dragSnapToOrigin 드래그 놓으면 가운데로 돌아감
+// dragElastic: DragElastic
+// 외부 제약 조건에서 허용되는 이동 정도. 0 = 움직임 없음, 1 = 전체 움직임. 기본 0.5
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover="hover"
-        whileDrag={{
-          backgroundColor: "rgb(112, 111, 211)",
-          transition: { duration: 10 },
-        }}
-        whileTap="click"
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={1}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
