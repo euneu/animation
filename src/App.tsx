aaefbaf71392f81c2a2f3388ea5066d563b0eea4
hanzii <pwe1ewq@gmail.com>
2022-08-20 00:18:23 +0900

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -13,77 +13,37 @@ const Wrapper = styled(motion.div)`
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
-  top: 100px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
-  display: flex;
-  position: absolute;
-  align-items: center;
-  justify-content: center;
-  font-size: 28px;
   border-radius: 40px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box = {
-  entry: (back: boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.5,
-    },
-  },
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.5,
-    },
-  }),
-};
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
-//custom
-//variants를 다르게 적용하고 싶을 때 사용
-
-// exitBeforeEnter
-// true로 설정하면 AnimatePresence는 한 번에 하나의 컴포넌트만 랜더링합니다.
-// exiting중인 컴포넌트는 entering하는 컴포넌트가 렌더링되기 전에 exit 애니메이션을 완료합니다.
-// 사용하지 않으면 entry와 exit가 동시에 움직임
-
+//layout css변화가 있으면 자동으로 animation 줌
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const nextPlease = () => {
-    setBack(false);
-    setVisible((current) => (current === 10 ? 10 : current + 1));
-  };
-  const prevPlease = () => {
-    setBack(true);
-    setVisible((current) => (current === 1 ? 1 : current - 1));
-  };
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((current) => !current);
   return (
-    <Wrapper>
-      <AnimatePresence custom={back}>
-        <Box
-          custom={back}
-          variants={box}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={prevPlease}>prev</button>
-      <button onClick={nextPlease}>next</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box
+        style={{
+          justifyContent: clicked ? "center" : "flex-start",
+          alignItems: clicked ? "center" : "flex-start",
+        }}
+      >
+        <Circle layout />
+      </Box>
     </Wrapper>
   );
 }
