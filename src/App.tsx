35cbdf9aff9e7978cb56bf4873c77f47feda5307
hanzii <pwe1ewq@gmail.com>
2022-08-19 20:18:22 +0900

    3. motionvalue- gradient, useScroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 import { motion, useMotionValue, useTransform } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useScroll } from "framer-motion";
+import { useEffect } from "react";
 
 //framer motion 설치
 // npm i framer-motion
 // import { motion } from "framer-motion";
 // <div></div> 이렇게 사용하면 안 되고 <motion.div></motion.div> 이렇게 사용해야 함
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 300vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -23,17 +24,6 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
 `;
 
 // Variants은 컴포넌트가 가질 수 있는 미리 정의된 시각적 state
@@ -82,17 +72,25 @@ const boxVariants = {
 // 점진적으로 다 얻고 싶음 => transformation
 // 그래서 사용하는거 useTransform
 // input과 output은 반드시 같은 배열 크기를 가져야 함
+// useScroll 스크롤 할 때마다 업데이트 되는 motionvalue 리턴 (number)
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 24, 238), rgb(0, 111, 238))",
+      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 99), rgb(194, 238, 0))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
 
-  useEffect(() => {
-    // x.onChange(()=>console.log(x.get()))
-    scale.onChange(() => console.log(scale.get()));
-  }, [x]);
   return (
-    <Wrapper>
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
