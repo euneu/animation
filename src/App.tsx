5f7001063bae847bd987d85748fb98086b6b986e
hanzii <pwe1ewq@gmail.com>
2022-08-20 00:30:23 +0900

    layoutId

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,8 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -17,8 +16,8 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
+  justify-content: center;
+  align-items: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -31,18 +30,21 @@ const Circle = styled(motion.div)`
 `;
 
 //layout css변화가 있으면 자동으로 animation 줌
+// layoutId FRAMER가 같은 UI CONPONENT를 묶어줌
 function App() {
   const [clicked, setClicked] = useState(false);
   const toggleClicked = () => setClicked((current) => !current);
   return (
     <Wrapper onClick={toggleClicked}>
-      <Box
-        style={{
-          justifyContent: clicked ? "center" : "flex-start",
-          alignItems: clicked ? "center" : "flex-start",
-        }}
-      >
-        <Circle layout />
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0 }} />
+        ) : null}
       </Box>
     </Wrapper>
   );
